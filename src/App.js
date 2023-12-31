1f7d77d9a1809ecdb034a85dd575ef22fb6d89ca
razack00 <121952296+razack00@users.noreply.github.com>
2024-01-01 00:00:59 +0100

    added routes, navbar component and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import './App.css';
+import './App.css'
+import Home from './pages/Home';
+// import Blog from './pages/Blog'
+// import data from "./data";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import { useEffect, useState } from 'react';
+// import Footer from './components/Footer';
+// import About from './pages/About'
+// import Contact from './pages/Contact'
+// import Details from './pages/Details';
+// import Reservation from './pages/Reservation'
 
 function App() {
+  const [blogs, setBlogs] = useState(null)
+  const [isPending, setIsPending] = useState(true)
+
+  const fetchblogs = async() => {
+    try{
+      const response = await fetch('http://localhost:3000/Blogs')
+      
+      if(!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`)
+      }
+      const data = await response.json()
+      setBlogs(data)
+      setIsPending(false)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchblogs()
+    console.log(blogs)
+  }, [])
   return (
-    <div className="App">
-      hello
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        {isPending && <div>Loading...</div>}
+        <Routes>
+          {blogs && <Route path='/viva-Express' element={<Home blogs={blogs} />} exact />}
+        </Routes> 
+        {/* <Footer/> */}
+      </Router>
+    </>
   );
 }
 
