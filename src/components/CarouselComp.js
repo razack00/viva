de4640bb8172a23f072873235e998c3209d2f595
razack00 <121952296+razack00@users.noreply.github.com>
2024-01-02 21:23:32 +0100

    carousel and footer comp

---
+++ src/components/CarouselComp.js
@@ -0,0 +1,51 @@
+import { Container } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
+
+function CarouselComp() {
+  return (
+    <div className='vh-100 w-100 mt-5 d-flex flex-column align-items-center justify-content-center gap-4'style={{background: '#BFC9D1', width: "100%"}}>
+      <h3 className='subtitle'>What Our Users Say</h3>
+      <Carousel className='bg-dark rounded-5 w-75 h-75 d-flex align-items-center'>
+        <Carousel.Item interval={3000}  >
+          <Carousel.Caption className=' w-75 position-static d-flex  align-items-center mx-auto '>
+            <div className='d-flex p-5 flex-column gap-5 align-items-center justify-content-center' style={{height: "fit-content"}}>
+              <img src={process.env.PUBLIC_URL + '/images/bus1.jpeg'} alt='Testimony' height="90px" width="90px" className='rounded-circle'></img>
+              <div>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum. la vitae elit libero, a pharetra auguela vitae elit libero, a pharetra auguela vitae elit libero, a pharetra augue</p>
+                <h3>Maria</h3>
+              </div>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={3000}  >
+          <Carousel.Caption className=' w-75 position-static d-flex p-5 align-items-center mx-auto '>
+            <div className='d-flex p-5 flex-column gap-5 align-items-center justify-content-center' style={{height: "fit-content"}}>
+              <img src={process.env.PUBLIC_URL + '/images/bus1.jpeg'} alt='Testimony' height="90px" width="90px" className='rounded-circle'></img>
+              <div>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum. la vitae elit libero, a pharetra auguela vitae elit libero, a pharetra auguela vitae elit libero, a pharetra augue</p>
+                <h3>Maria</h3>
+              </div>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={3000}  >
+          <Carousel.Caption className=' w-75 position-static d-flex p-5 align-items-center mx-auto '>
+            <div className='d-flex p-5 flex-column gap-5 align-items-center justify-content-center' style={{height: "fit-content"}}>
+              <img src={process.env.PUBLIC_URL + '/images/bus1.jpeg'} alt='Testimony' height="90px" width="90px" className='rounded-circle'></img>
+              <div>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum. la vitae elit libero, a pharetra auguela vitae elit libero, a pharetra auguela vitae elit libero, a pharetra augue</p>
+                <h3>Maria</h3>
+              </div>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+    
+  );
+}
+
+export default CarouselComp;
---
+++ src/components/Footer.js
@@ -0,0 +1,48 @@
+import { Row, Col, Container} from 'react-bootstrap'
+
+function Footer() {
+    return (
+    <div className='bg-dark py-5 footer'  data-bs-theme="dark">
+        <Container className='text-white' data-bs-theme="dark">
+            <Row>
+                <Col className='text-left pt-4' sm={6} md={4} lg={3}>
+                    <h2>Bus Travel</h2>
+                    <ul className='p-0'>
+                        <li><a href='#home'>All Buses</a></li>
+                        <li><a href='#home'>All Bus routes</a></li>
+                        <li><a href='#home'>Discounts and Promotions</a></li>
+                    </ul>
+                </Col >
+                <Col className='text-left pt-4' sm={6} md={4} lg={3}>
+                    <h2>Quick Links</h2>
+                    <ul className='p-0'>
+                        <li><a href='#home'>Services</a></li>
+                        <li><a href='#home'>Rent a Bus</a></li>
+                        <li><a href='#home'>Reservation</a></li>
+                    </ul>
+                </Col >
+                <Col className='text-left pt-4' sm={6} md={4} lg={3}>
+                    <h2>Experience Viva</h2>
+                    <ul className='p-0'>
+                        <li><a href='#home'>About Us</a></li>
+                        <li><a href='#home'>Our Organisation</a></li>
+                        <li><a href='#home'>Media</a></li>
+                        <li><a href='#home'>Jobs and Careers</a></li>
+                    </ul>
+                </Col >
+                <Col className='text-left pt-4' sm={6} md={4} lg={3}>
+                    <h2>Customer Service</h2>
+                    <ul className='p-0'>
+                        <li><a href='#home'>Help</a></li>
+                        <li><a href='#home'>Manage My Booking</a></li>
+                        <li><a href='#home'>Support</a></li>
+                    </ul>
+                </Col >
+            </Row>
+            <div className='text-center pt-5'>Copyright 2023 @Razack</div>
+        </Container>
+    </div>
+  );
+}
+
+export default Footer;
