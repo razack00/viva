105d8f77d6d5e12a26a755b30aa557c68b6daa1d
razack00 <121952296+razack00@users.noreply.github.com>
2024-01-02 21:22:33 +0100

    blog comp

---
+++ src/components/BlogCard.js
@@ -0,0 +1,20 @@
+
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+function BlogCard({post}) {
+  return (
+    <Card className='mt-5'>
+      <Card.Img variant="top" src={process.env.PUBLIC_URL + post.img}  height="220"/>
+      <Card.Body>
+        <Card.Title>{post.title}</Card.Title>
+        <Card.Text>
+         {post.body.slice(0, 100)}... 
+        </Card.Text>
+        <Link to={`/blogs/${post.id}`}> Read More </Link> 
+      </Card.Body>
+    </Card>    
+  );
+}
+
+export default BlogCard;
