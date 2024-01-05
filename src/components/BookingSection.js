656d7c429dfbc17068acf86c55653ee1aeb70daf
razack00 <121952296+razack00@users.noreply.github.com>
2024-01-05 23:55:43 +0100

    added props to formData, edited onChange event handlers and edited some styling

--- src/components/BookingSection.js
+++ src/components/BookingSection.js
@@ -5,17 +5,21 @@ import { useNavigate } from 'react-router-dom';
 // import { getDefaultNormalizer } from '@testing-library/react';
 
 function BookingSection() {
-    const [origin, setOrigin] = useState("")
-    const [destination, setDestination] = useState("")
     const [show, setShow] = useState(false);
     const [availableRoutes, setavailableRoutes] = useState([]);
     const [message, setmessage] = useState([])
-    // const [route, setRoute] = useState([])
     const [formData, setFormData] = useState({
-        category: "",
+        origin: "",
+        destination: "",
+        category: "All",
+        departure_date: "",
         NumPassengers: "",
-        tripType: "" 
+        tripType: "",
+        returnDate: ""
     })
+
+    const today = new Date()
+    const formattedToday = today.toISOString().slice(0, 10)
 
     const navigate = useNavigate()
 
@@ -23,42 +27,34 @@ function BookingSection() {
     const fetchRoutes = async() => {
         try{
           const response = await fetch('http://localhost:8000/Routes')
+
+          //Throwing a custom error if the status code is not ok(200)   
           if(!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`)
           }
           const data = await response.json()
 
-          //filtering available routes routes
-          setavailableRoutes(data.filter(route => route.origin === origin && route.destination === destination))
+          //filtering available routes 
+          setavailableRoutes(data.filter(route => {
+            if(formData.category == "All") {
+                return route.origin === formData.origin && route.destination === formData.destination
+            }
+                return route.origin === formData.origin && route.destination === formData.destination && formData.category === route.category
+            }))
           console.log(availableRoutes)
         } catch (err) {
           console.log(err)
         }
     }
     
+    // rerangering when from input state changes
     useEffect(() => {
         fetchRoutes()
         console.log('render')
         console.log('info:', formData)
 
-    }, [origin, destination, formData])
-
-    // updating states ( origin and destination)
-    const onOriginChange = (e) => {
-        setOrigin(e.target.value)
-    }
-    const onDestinationChange = (e) => {
-        setDestination(e.target.value)
-    }
-    const onCategoryChange = (e) => {
-        setFormData({ ...formData, category: e.target.value });
-    }
-    const onNumPassengerChange = (e) => {
-        setFormData({ ...formData, NumPassengers: e.target.value });
-    }
-    const onTripTypeChange = (e) => {
-        setFormData({ ...formData, tripType: e.target.value });
-    }
+    }, [formData])      
+ 
 
     // handling Modal display
     const handleClose = () => setShow(false);
@@ -67,26 +63,30 @@ function BookingSection() {
     // validating form submittion
     const handleSubmit = (event) => {
         event.preventDefault()
-        if(availableRoutes.length > 0) {
+        const isEmpty = Object.values(formData).some(value => value === "")
+        console.log("is empty", isEmpty)
+        if(isEmpty){
+            setmessage("Please fill in all fields")
+        }else if(formData.origin === formData.destination) {
+            setmessage("Invalid Route. Choose a valid route")
+        }else if(availableRoutes.length > 0) {
             handleShow()
             setmessage([])
-        }else if(origin === destination) {
-            setmessage("Invalid Route. Choose a valid route")
-        }else {
+        }else{
             setmessage("No bus for choosen Route") 
         }
     }
 
     return (
     <Container className='bg-white rounded-4 px-5 py-4 shadow' style={{marginTop: "-85px"}}>
-        <Form className='d-flex flex-column gap-5 py-4' onSubmit={handleSubmit}>
+        <Form className='d-flex flex-column justify-content-center gap-5 py-4' onSubmit={handleSubmit}>
             <div className='text-danger'>{message}</div>
             <Row className='d-flex'>
                 <Col>
                     <Form.Label htmlFor="from">Origin:</Form.Label>
                     <Form.Select 
-                        onChange={onOriginChange}
-                        value={origin}
+                        onChange={e => setFormData({...formData, origin: e.target.value})}
+                        value={formData.origin}
                         id='from'>
                             <option>Enter origin</option>
                             <option value="Yaounde">Yaounde</option>
@@ -96,8 +96,8 @@ function BookingSection() {
                 <Col>
                     <Form.Label htmlFor="to">Destination:</Form.Label>
                     <Form.Select 
-                        onChange={onDestinationChange}
-                        value={destination}
+                        onChange={e => setFormData({...formData, destination: e.target.value})}
+                        value={formData.destination}
                         >
                             <option>Enter destination</option>
                             <option value="Yaounde">Yaounde</option>
@@ -105,66 +105,91 @@ function BookingSection() {
                     </Form.Select>
                 </Col>
                 <Col>
-                    <Form.Label htmlFor="type">Type:</Form.Label>
-                    <Form.Select name='type' onChange={onCategoryChange}>
-                        <option>Select Class</option>
+                    <Form.Label htmlFor="type">Category:</Form.Label>
+                    <Form.Select name='type' onChange={e => setFormData({ ...formData, category: e.target.value })}>
+                        <option>All</option>
                         <option value="Economic">Economic</option>
                         <option value="First Class">First Class</option>
                         <option value="Classic">Classic</option>
                     </Form.Select>
                 </Col>
                 <Col>
-                    <Form.Label htmlFor="date">Date of Departure:</Form.Label>
+                    <Form.Label htmlFor="departure-date">Departure Date:</Form.Label>
                     <Form.Control 
                     className='fs-5' 
                     style={{paddingBlock: "4.92px",}} 
-                    type="date" 
-                    name='date'/>
+                    type="date"
+                    onChange={e => setFormData({...formData, departure_date: e.target.value})}
+                    min = {formattedToday} 
+                    name='departure-date'/>
                 </Col>
             </Row>
-            <Row className='d-flex g-0 justify-content-between w-100 gap-5 m-0'>
-                <div className='d-flex gap-5' style={{width: "45.5%"}}>
-                    <Col>
-                        <Form.Label htmlFor="date">No. of Passengers:</Form.Label>
-                        <Form.Control 
-                            defaultValue={0}  
+            <Row className='d-flex w-100 m-0 g-0 gap-4'>
+                <Col className='d-flex'>
+                    <Row className='g-0 gap-4 w-100'>
+                        <Col>
+                            <Form.Label htmlFor="date">No. of Passengers:</Form.Label>
+                            <Form.Control 
+                                defaultValue={0}  
+                                className='fs-5' 
+                                type="number"
+                                onChange={e => setFormData({ ...formData, NumPassengers: e.target.value })} 
+                                name='date' 
+                                />
+                        </Col>
+                        <Col>
+                            <Form.Label>Trip Type</Form.Label>
+                            <Form.Group className='d-flex align-self-end py-2 justify-content-around gap-2'>
+                                <Form.Check
+                                    inline
+                                    type="radio"
+                                    id="radio"
+                                    onChange={ e => setFormData({ ...formData, tripType: e.target.value, returnDate: "" }) }
+                                    label="One Way"
+                                    value= "One Way"
+                                    name = "One"
+                                />
+                                <Form.Check
+                                    inline
+                                    type="radio"
+                                    id="radio"
+                                    onChange={ e => setFormData({ ...formData, tripType: e.target.value })}
+                                    label="Round Trip"
+                                    value= "Round Trip"
+                                    name = "One"
+                                />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col>
+                    <Row className='g-0 gap-4'>
+                        {formData.tripType === "Round Trip" &&  <Col>
+                            <Form.Label htmlFor="return-date">return Date:</Form.Label>
+                            <Form.Control 
                             className='fs-5' 
-                            type="number"
-                            onChange={onNumPassengerChange} 
-                            name='date' 
-                            style={{paddingBlock: "4px"}}/>
-                    </Col>
-                    <Form.Group className='align-self-end'>
-                        <Form.Check
-                            inline
-                            type="radio"
-                            id="radio"
-                            onChange={onTripTypeChange}
-                            label="One Way"
-                            value= "One Way"
-                            name = "One"
-                           />
-                        <Form.Check
-                            inline
-                            type="radio"
-                            id="radio"
-                            onChange={onTripTypeChange}
-                            label="Round Trip"
-                            value= "Round Trip"
-                            name = "One"
-                        />
-                    </Form.Group>
-                </div>
-                <div className='p-0 text-center' style={{width: "23.6%",}}>
-                    <Button type='submit' style={{width: "100%", paddingBlock: "10px", margin: '0'}} variant="primary">
-                        Search
-                    </Button>
-                </div>
+                            style={{paddingBlock: "4.92px",}} 
+                            type="date"
+                            onChange={e => setFormData({...formData, returnDate: e.target.value})}
+                            min = {formattedToday} 
+                            name='return-date'/>
+                        </Col>}
+                        <Col className='bg-white'>
+                            <div className='p-4'></div>
+                            <Button className='mx-auto' type='submit' style={{width: "248px", paddingBlock: "8.3px", margin: '0'}} variant="primary">
+                                Search
+                            </Button>
+                        </Col>
+                    </Row>
+                </Col>
             </Row>
+
+            {/* pop up to the list of available routes and buses */}
             <Modal className='modal-lg' show={show} onHide={handleClose}>
                 <Modal.Header closeButton>Available buses for your Search
                 </Modal.Header>
                 <Modal.Body className='px-5 py-4'>
+                    {/* looping through the list of available routes and displaying */}
                     {availableRoutes.map(route => (
                         <Row key={route.id} className='border py-3'>
                             <Col> 
@@ -179,6 +204,7 @@ function BookingSection() {
                             </Col>
                             <Col>
                                 <li>Price: {route.price}</li>
+                                {/* a choose btn which takes users to reservation page along with data using useNavigation() */}
                                 <Button 
                                     style={{width: "120px"}} 
                                     variant="primary"
