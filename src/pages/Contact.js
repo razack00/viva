8476190e529950902b6c204d66e8050e7f78611e
razack00 <121952296+razack00@users.noreply.github.com>
2024-01-18 22:27:09 +0100

    added a contact form

---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+import ContactForm from '../components/ContactForm'
+
+function Contact() {
+  return (
+    <div className='page-container'>
+         <h1 className="py-5 text-center">Contact Us</h1>
+         <ContactForm />
+    </div>
+  );
+}
+
+export default Contact;
