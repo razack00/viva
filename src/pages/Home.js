d9df31eba2f53dda429ac7cf5059c2aaf781a501
razack00 <121952296+razack00@users.noreply.github.com>
2024-04-05 19:06:13 +0100

    now using useData hook instead of props to display blogs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,29 +4,35 @@ import TextCard from "../components/TextCard"
 import BlogCard from "../components/BlogCard"
 import Showcase from "../components/Showcase"
 import CarouselComp from "../components/CarouselComp"
+import RoutesAndPrices from "../components/RoutesAndPrices"
+import { useData } from "../context/DataContext"
 
-function Home({blogs}) {
+function Home() {
+    const {blogs, isPending, err} = useData()
     return (
         <div className="page-container">
             <header>
                 <Container className="h-100 align-items-center d-flex">
-                    <div className="w-50 gap-3 m-0" style={{}}>
+                    <Col md="6" className="gap-3 m-0" >
                         <p className="text-white fs-2 fw-bold">Plan Your Trip Now</p>
                         <h1 className="title hero-title mb-4">Travel in <span className="fancy">Comfort</span> with Viva express</h1>
                         <h2 className="hero-subtitle">
                             Rent the car of your dreams. Unbeatable prices, unlimited miles, flexible pick-up options and much more.
                         </h2>
-                    </div>
+                    </Col>
                 </Container>
             </header>
             <BookingSection/>
             <TextCard />
             <Showcase />
+            <RoutesAndPrices />
             <Container>
                 <Row>
+                    {isPending && <div>Loading...</div>}
+                    {err && <div>{err}</div>}
                     {blogs && blogs.slice(0, 6).map(blog => (
                         <Col id={blog.id} sm={6} md={4} lg={4}>
-                            <BlogCard post = {blog} />
+                            <BlogCard blog = {blog} />
                         </Col>
                     ))}
                 </Row>
