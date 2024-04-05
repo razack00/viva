f03e112fd1545cd5076e8ecaad163d233a9a4e2b
razack00 <121952296+razack00@users.noreply.github.com>
2024-04-05 19:06:54 +0100

    displaying blog details for selected blog

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,26 +1,30 @@
+import { useEffect, useState } from "react";
 import { Container, Col } from "react-bootstrap";
-import data from "../data";
 import { useParams } from "react-router-dom";
+import { useData } from "../context/DataContext";
 
 function Details() {
   const { id } = useParams();
-  const post = data.find((d) => {
-    console.log(d.id === parseInt(id))
-    return d.id === parseInt(id)
-});
+  const {blogs, isPending, err} = useData()
+  const [blog, setblog] = useState()
 
-  if (!post) {
-    return <h3 className="text-center">Post not found</h3>; // Handle missing post
-  }
+  useEffect(() => {
+    const matchedBlog = blogs && blogs.find((blog) => {
+      return blog.id === parseInt(id)
+    });
+    setblog(matchedBlog)
+  },[blogs, id])
 
-  return (
+  return ( 
     <div className="page-container">
       <Container>
         <h1 className="py-5 text-center">blog</h1>
-        <Col>
-          <h2 className="pb-5 fs-1">{post.title}</h2>
-          <p className="pb-5">{post.body}</p>
-        </Col>
+        {isPending && <div>Loading...</div>}
+        {err && <div>{err}</div>}
+        {blog && <Col>
+          <h2 className="pb-5 fs-1">{blog.title}</h2>
+          <p className="pb-5">{blog.body}</p>
+        </Col>}
       </Container>
     </div>
   );
