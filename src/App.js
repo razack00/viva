7f9cb0c0e8adc530c39d9ed3c10ba3f6ca69d7a9
razack00 <121952296+razack00@users.noreply.github.com>
2024-04-05 21:10:10 +0100

    moved data fetching to useData and usefetch and added some routes

--- src/App.js
+++ src/App.js
@@ -1,50 +1,40 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css'
 import Home from './pages/Home';
-// import Blog from './pages/Blog'
-// import data from "./data";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import { useEffect, useState } from 'react';
 import Footer from './components/Footer';
 import About from './pages/About'
 import Contact from './pages/Contact'
 // import Details from './pages/Details';
 import Reservation from './pages/Reservation'
+import TransportServices from './pages/TransportServices';
+import CourierServices from './pages/CourierServices';
+import ReservationDetails from './pages/ReservationDetails';
+import Blog from './pages/Blog';
+import Details from './pages/Details';
+import { ContextProvider } from './context/DataContext';
 
 function App() {
-  const [blogs, setBlogs] = useState(null)
-  const [isPending, setIsPending] = useState(true)
 
-  const fetchblogs = async() => {
-    try{
-      const response = await fetch('http://localhost:8000/Blogs')
-      if(!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`)
-      }
-      const data = await response.json()
-      setBlogs(data)
-      setIsPending(false)
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  useEffect(() => {
-    fetchblogs()
-  }, [])
   return (
     <>
       <Router>
-        <NavBar />
-        {isPending && <div>Loading...</div>}
-        <Routes>
-          <Route path='/viva-Express' element={<Home blogs={blogs} />} exact />
-          <Route path='/reservation' element={<Reservation />} />
-          <Route path='/about' element={<About />} />
-
-          <Route path='/contact' element={<Contact />} />
-        </Routes> 
-        <Footer/>
+        <ContextProvider>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<Home />} exact />
+            <Route path='/reservationdetails' element={<ReservationDetails />} />
+            <Route path='/about' element={<About />} />
+            <Route path='services/transportServices' element={<TransportServices />} />
+            <Route path='/services/CourierServices' element={<CourierServices />} />
+            <Route path='/details/:id' element={<Details/>} />
+            <Route path='/reservation' element={<Reservation />} />
+            <Route path='/blog' element={<Blog />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes> 
+          <Footer/>
+        </ContextProvider>
       </Router>
     </>
   );
